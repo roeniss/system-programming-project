d2a5c36b90936066c2eb3bf79f252bb7c8f09a5b
Roeniss Moon <roeniss2@gmail.com>
2020-03-19 04:19:31 -0700

    add get_input function

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 char input[100];
 
@@ -6,14 +7,25 @@ void get_input();
 
 int main()
 {
-    get_input();
-
-    printf("--->%s<---", input);
-    return 0;
+	get_input();
+	printf("--->%s<---", input);
+	get_input();
+	printf("--->%s<---", input);
+	get_input();
+	printf("--->%s<---", input);
+	return 0;
 }
 
+// read (input - 1) size's stdin
 void get_input()
 {
-    printf("sicsim > ");
-    fgets(input, sizeof(input), stdin);
+	int c;
+	printf("sicsim > ");
+	// strlen(input) become read data size. (maxLen: char array size - 1)
+	// (NULL is not counted)
+	// but in this case, (max : char array size -2) reason : check below.
+	fgets(input, sizeof(input), stdin);
+	while (input[strlen(input) - 1] != '\n' && (c = getchar()) != '\n')
+		;
+	input[strlen(input) - 1] = '\0'; // remove last lineberak --> maxLen -= 1
 }
