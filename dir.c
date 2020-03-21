74ccef8e2a1abccfb2c598d50db7b988c7a739a2
Roeniss Moon <roeniss2@gmail.com>
2020-03-21 02:57:35 -0700

    use standard method for check-directory

--- dir.c
+++ dir.c
@@ -12,13 +12,17 @@ void dir()
 	struct dirent *dd;
 	struct stat buf;
 
-	for (dd = readdir(dr); dd; dd = readdir(dr))
+	int no = 0;
+	for (dd = readdir(dr); dd; dd = readdir(dr), ++no)
 	{
-		printf("%-10s", dd->d_name);
+		printf("%10s", dd->d_name);
 		stat(dd->d_name, &buf);
-		if (dd->d_type & DT_DIR)
+
+		if (no < 2)
+			printf(" ");
+		else if (S_ISDIR(buf.st_mode))
 			printf("/ ");
-		else if (buf.st_mode & S_IXUSR && strcmp(".", dd->d_name) && strcmp("..", dd->d_name))
+		else if (buf.st_mode & S_IXUSR || buf.st_mode & S_IXGRP || buf.st_mode & S_IXOTH)
 			printf("* ");
 		else
 			printf(" ");
