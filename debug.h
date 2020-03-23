942d8f4c9f19a4519ac7604b74adb25a3f8aa431
Roeniss Moon <roeniss2@gmail.com>
2020-03-23 04:40:25 -0700

    add comments for functions

--- debug.h
+++ debug.h
@@ -3,8 +3,10 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
+// check main parameter if exist
 extern void check_debug_mode(char *[]);
 
+// used to determine whethere print DEBUG message or not
 extern bool is_debug_mode();
 
 #endif
