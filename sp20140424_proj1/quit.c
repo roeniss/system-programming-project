e634ca87bb4457c40ec605d80bf89099b930f666
Roeniss Moon <roeniss2@gmail.com>
2020-03-30 19:26:22 +0900

    remove duplcated files

--- sp20140424_proj1/quit.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-
-#include "quit.h"
-
-int quit(void)
-{
-	exit(0);
-	return 0;
-}
--- sp20140424_proj1/reset.h
+++
@@ -1,9 +0,0 @@
-#ifndef RESET_H
-#define RESET_H
-
-//
-// Reset the virtual memory.
-//
-int reset(void);
-
-#endif
