efcdd83a22614b0bca1f841253048fc0fe631ae5
Roeniss Moon <roeniss2@gmail.com>
2020-03-31 02:31:02 +0900

    add document for explanation

--- makeFinalResult.sh
+++ makeFinalResult.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-cd sp20140424_proj1  && \
-rm -rf *  && \
-cd ..  && \
-cp *.c ./sp20140424_proj1/  && \
-cp *.h ./sp20140424_proj1/  && \
-cp *.docx ./sp20140424_proj1/  && \
-cp opcode.txt ./sp20140424_proj1/  && \
-cp Makefile ./sp20140424_proj1/  && \
-cp README.md ./sp20140424_proj1/README  && \
-cd sp20140424_proj1  && \
+mkdir tmp
+cd tmp && \
+rm -rf * && \
+cd .. && \
+cp *.c ./tmp/ && \
+cp *.h ./tmp/ && \
+cp *.docx ./tmp/ && \
+cp opcode.txt ./tmp/ && \
+cp Makefile ./tmp/ && \
+cp README.md ./tmp/README && \
+cd tmp && \
 make clean && make && make clean && tar cvf sp20140424_proj1.tar * && \
 ls | grep -v sp20140424_proj1.tar | xargs rm 
