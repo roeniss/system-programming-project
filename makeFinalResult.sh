782d340e9b664428b01963cceda77e3387eb06e6
RoenissMoon <roeniss2@gmail.com>
2020-04-02 15:46:04 +0900

    revise script that makes tar file

--- makeFinalResult.sh
+++ makeFinalResult.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-mkdir tmp
-cd tmp && \
-rm -rf * && \
-cd .. && \
-cp *.c ./tmp/ && \
-cp *.h ./tmp/ && \
-cp *.docx ./tmp/ && \
-cp opcode.txt ./tmp/ && \
-cp Makefile ./tmp/ && \
-cp README.md ./tmp/README && \
-cd tmp && \
-make clean && make && make clean && tar cvf sp20140424_proj1.tar * && \
-ls | grep -v sp20140424_proj1.tar | xargs rm 
+# variable
+DIR=sp20140424_proj1
+
+# code
+make clean && \
+
+mkdir $DIR && \
+
+cp README.md $DIR/ && \
+mv $DIR/README.md $DIR/README && \
+
+cp *.c $DIR/ && \
+cp *.h $DIR/ && \
+cp *.docx $DIR/ && \
+cp Makefile $DIR/  && \
+cp opcode.txt $DIR/ && \
+
+tar cvf $DIR.tar $DIR/ && \
+rm -rf $DIR/ && \
+
+echo done
